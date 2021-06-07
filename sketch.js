052cca38849eaaf7751ecb4d0e9da0c5ac184690
ga-fleury <gabrielfleury95@gmail.com>
2021-06-07 10:12:27 -0300

    Hello World

--- sketch.js
+++ sketch.js
@@ -1,68 +1,112 @@
-let bubbles = [];
-let h = 400;
-let w = 400;
+var nSteps = 32;
+var nTracks = 4;
+var kit;
+var cellWidth, cellHeight;
+var beats = 0;
+var cells = [];
+var currentStep = 0;
+var playButton;
+var margin = 12;
+var trackColors = [
+    '#FBD1A2',
+    '#7DCFB6',
+    '#00B2CA',
+    '#1d4e89'
+]
 
+var drumNames = ["hho", "hh", "snare", "kick"];
+kit = new Tone.Players(
+  {"hho" : "./samples/505/hho.mp3",
+		"hh" : "./samples/505/hh.mp3",
+    "snare" : "./samples/505/snare.mp3",
+    "kick" : "./samples/505/kick.mp3"
+  });
 
+kit.toMaster();
+Tone.Transport.bpm.value = 210;
+Tone.Transport.scheduleRepeat(onBeat, "8n");
 
-function setup() {
-  createCanvas(h, w);
-    for (let i = 0; i < 50; i++) {
-    let r = random(10);
-    let x = 200;
-    let y = 200;
-    let time = random(7);
-    let vx = random(-3, 3);
-    let vy = random(-3, 3);
-    bubbles[i] = new Bubble(x,y,r,vx,vy,time);
+function setup(){
+  createCanvas(1060,120);
+	cellWidth = (width-margin)/nSteps;
+	cellHeight = (height-margin)/nTracks;
+  
+  for(var track=0; track<nTracks; track++){
+  	cells[track] = [];
+    for(var step=0; step<nSteps; step++){
+    	cells[track][step] = -1;
     }
-  print(bubbles[0])
+  }  
+  playButton = createButton('Play');
+  // playButton.width(displayWidth/2, 300);
+  playButton.mouseClicked(togglePlay);
 }
 
-function draw() {
-  background(0);
-  for (let i = 0; i < bubbles.length; i++) {
-    bubbles[i].show();
-    bubbles[i].move();
-    bubbles[i].out();
+function onBeat(time){
+	for(var track=0; track<nTracks; track++){
+  	if(cells[track][currentStep] == 1){
+    	var drum = kit.get(drumNames[track]);
+      drum.start(time);
+    }
+  }
+  beats++;
+  currentStep = (beats) % nSteps;
+
+  console.log(beats, currentStep);
+}
+
+function draw(){
+	background(255);
+  stroke(0);
+	
+  for(var track=0; track<nTracks; track++){
+  	for(var step=0; step<nSteps; step++){
+          if(step % 4 == 0){
+              fill(244);
+              rect(step*cellWidth,track*cellHeight,cellWidth, cellHeight);
+          }
+  		if(cells[track][step] == 1){
+        fill(trackColors[track]);
+        rect(step*cellWidth,track*cellHeight,cellWidth, cellHeight);
+      }
+  	}
+  }
+  
+  //vertical lines
+  for(var i=0; i<=nSteps; i++){
+  	line(i*cellWidth,0,i*cellWidth,height-margin);
+  }
+  
+  //horizontal lines
+  for(var j=0; j<=nTracks; j++){
+  	line(0,j*cellHeight,width-margin,j*cellHeight);
+  }
+  
+  highlight = (beats-1) % nSteps;
+    fill('rgba(255,0,0, 0.25)')
+	noStroke();
+	rect(highlight*cellWidth, 0, cellWidth, height);
+  
+}
+
+function mousePressed(){
+	if(0<mouseX && mouseX<width && 
+     0<mouseY && mouseY<height){
+    
+  	var i = floor(mouseX/cellWidth);
+    var j = floor(mouseY/cellHeight);
+    
+    cells[j][i] = -cells[j][i];
   }
 }
 
-
-class Bubble {
-  constructor(x, y, r, vx, vy, time) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.vx = vx;
-    this.vy = vy;
-    this.time = time;
-  }
-  
-  show() {
-        this.time = this.time - 0.1;
-    if(this.time < 0) {
-    stroke(255);
-    strokeWeight(4);
-    noFill();
-    ellipse(this.x, this.y, this.r * 2)
-    }
-  }
-  
-  move() {
-    this.time = this.time - 0.1;
-    if(this.time < 0) {
-    this.x += this.vx;
-    this.y += this.vy;
-    }
-  }
-  
-  out() {
-      if(this.x > w || this.y > h || this.x < 0 || this.y < 0) {
-      this.x = 200;
-      this.y = 200;
-      this.vx = random(-3, 3);
-      this.vy = random(-3, 3)
-      this.r = random(10);
-    }
+function togglePlay(){
+	if(Tone.Transport.state == "started"){
+  	Tone.Transport.stop();
+    playButton.html('Play');
+  } else {
+    beats = -1;
+  	Tone.Transport.start();
+    playButton.html('Stop');
   }
 }
